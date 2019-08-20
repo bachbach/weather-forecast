2bc9a8cbdb80acf2b655f24cca29f38534590edf
Barbara Pietrzyk <b.pietrzyk90@gmail.com>
2019-08-20 20:00:29 +0200

    Updates

---
+++ precache-manifest.b686714be873e3e4791b4cfd82ca3a3d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a47140646bc00662a1e6f29a2054c20a",
+    "url": "/weather-forecast/index.html"
+  },
+  {
+    "revision": "1e7adbdfc559a110bf31",
+    "url": "/weather-forecast/static/css/main.8e974a6f.chunk.css"
+  },
+  {
+    "revision": "622700d0a41c32fa43be",
+    "url": "/weather-forecast/static/js/2.63af959d.chunk.js"
+  },
+  {
+    "revision": "1e7adbdfc559a110bf31",
+    "url": "/weather-forecast/static/js/main.7f391101.chunk.js"
+  },
+  {
+    "revision": "6a504033b2c3bdfefdc1",
+    "url": "/weather-forecast/static/js/runtime~main.a5b7c690.js"
+  }
+]);

161528808aad7eca1aed11b8c9c5c8ba01df6f67
Barbara Pietrzyk <b.pietrzyk90@gmail.com>
2019-08-20 19:58:34 +0200

    Updates

---
+++ precache-manifest.c427be7810d81ac66e1ba6a1855215ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d5ddab5590e567b174614f72883e991",
+    "url": "/index.html"
+  },
+  {
+    "revision": "875add755011d07e2ea2",
+    "url": "/static/css/main.8e974a6f.chunk.css"
+  },
+  {
+    "revision": "622700d0a41c32fa43be",
+    "url": "/static/js/2.63af959d.chunk.js"
+  },
+  {
+    "revision": "875add755011d07e2ea2",
+    "url": "/static/js/main.cdf405ef.chunk.js"
+  },
+  {
+    "revision": "bfae5c69e7f7833ceeb6",
+    "url": "/static/js/runtime~main.83e0f0f3.js"
+  }
+]);
